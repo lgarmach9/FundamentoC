58c2772085f379499cb424d6e43a0f663539a95a
lgarmach9 <118854437+lgarmach9@users.noreply.github.com>
2023-11-07 23:29:30 +0100

    Add files via upload

---
+++ 002-Comentarios/003-multipleslineas.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc,char *argv[]){
+    /*
+    
+        Esto es una linea de comentario
+        Estos es otra linea de comentario
+        Tercera linea de comentario
+    
+    */
+    return 0;
+}
