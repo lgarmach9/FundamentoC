3ffc3b8f251d3836698653845aa31dfc8b89045a
lgarmach9 <118854437+lgarmach9@users.noreply.github.com>
2023-11-07 23:38:20 +0100

    Create 001-arrays.c

---
+++ 006-Estructuras de datos/001-arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc,char *argv[]){
+    int telefonos[20];
+    
+    telefonos [0]= 12345;
+    telefonos [1]= 123456;
+    telefonos [2]= 5455631;
+    telefonos [3]= 464846;
+    
+    printf("El primer telefono es: %i \n", telefonos[0]);
+        
+    return 0;
+}
